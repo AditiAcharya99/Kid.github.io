e78bcb7d9027ae7970a27bdb24a9f364819c9e9b
Aditi Acharya <aditiacharya99@gmail.com>
2020-08-01 20:30:08 +0530

    Add project files.

---
+++ Calculator/Default.aspx
@@ -0,0 +1,154 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calculator._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Calculator</h1>
+        <p class="lead">We encourage you to do it mentally but always have your back!</p>
+        <p><a href="https://www.youtube.com/channel/UCosJ16GN2g2tGsVQnGK9pvA" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="1" Width="150px" />
+            </h2>
+            </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="2" Width="150px" />
+            </h2>
+            
+        </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="3" Width="150px" />
+            </h2>
+           
+        </div>
+        
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="+" Width="150px" />
+            </h2>
+           
+        </div>
+
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button17" runat="server" OnClick="Button17_Click" Text="Delete" Width="150px" />
+            </h2>
+           
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="4" Width="150px" />
+            </h2>
+         </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="5" Width="150px" />
+            </h2>
+            
+        </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="6" Width="150px" />
+            </h2>
+           
+        </div>
+        
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button8" runat="server" OnClick="Button8_Click" Text="-" Width="150px" />
+            </h2>
+            </div>
+         <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button18" runat="server" OnClick="Button18_Click" Text="C" Width="150px" />
+            </h2>
+           
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button9" runat="server" OnClick="Button9_Click" Text="7" Width="150px" />
+            </h2>
+            
+                    </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button10" runat="server" OnClick="Button10_Click" Text="8" Width="150px" />
+            </h2>
+            
+        </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button11" runat="server" OnClick="Button11_Click" Text="9" Width="150px" />
+            </h2>
+           
+        </div>
+        
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button12" runat="server" OnClick="Button12_Click" Text="x" Width="150px" />
+            </h2>
+           
+        </div>
+         <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button19" runat="server" OnClick="Button19_Click" Text="!" Width="150px" />
+            </h2>
+           
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button13" runat="server" Text="." Width="150px" OnClick="Button13_Click" />
+            </h2>
+            
+                    </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button14" runat="server" OnClick="Button14_Click" Text="0" Width="150px" />
+            </h2>
+            
+        </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button15" runat="server" Text="=" Width="150px" OnClick="Button15_Click" />
+            </h2>
+           
+        </div>
+        
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button16" runat="server" OnClick="Button16_Click" Text="/" Width="150px" />
+            </h2>
+           
+        </div>
+        <div class="col-md-2">
+            <h2>
+                <asp:Button ID="Button20" runat="server" OnClick="Button16_Click" Text="Sqrt" Width="150px" />
+            </h2>
+           
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12"
+            <p class="lead"><asp:Label ID="Label1" runat="server" Text=".." BorderStyle="Double" Width="100px" BackColor="#CCCCFF" Font-Bold="True" Font-Size="20pt" Height="40px"></asp:Label>
+                RESULT = 
+            <asp:Label ID="Label2" runat="server" Text=".." BorderStyle="Double" Width="100px" BackColor="#CCCCFF" BorderColor="Black" Font-Bold="True" Font-Size="20pt" Height="40px"></asp:Label>
+                </p>
+            </div>
+
+    </div>
+</asp:Content>
---
+++ Calculator/Default.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["Operand"] = null;
+                Session["Num1"] = null;
+                Session["Num2"] = null;
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "1";
+            else Session["Num2"] += "1";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "2";
+            else Session["Num2"] += "2";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "3";
+            else Session["Num2"] += "3";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "4";
+            else Session["Num2"] += "4";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "5";
+            else Session["Num2"] += "5";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "6";
+            else Session["Num2"] += "6";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "7";
+            else Session["Num2"] += "7";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "8";
+            else Session["Num2"] += "8";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "9";
+            else Session["Num2"] += "9";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += "0";
+            else Session["Num2"] += "0";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Session["Operand"] = "+";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            Session["Operand"] = "-";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            Session["Operand"] = "x";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button19_Click(object sender, EventArgs e)
+        {
+            Session["Operand"] = "!";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            Session["Operand"] = "/";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            decimal Num1;
+            decimal Num2 = 0;
+            decimal res = 1;
+
+            Num1 = Convert.ToDecimal(Session["Num1"].ToString());
+            if(Session["Num2"]!=null) Num2 = Convert.ToDecimal(Session["Num2"].ToString());
+            if (Session["Operand"] != null && Session["Operand"].ToString() == "+")
+            {
+                res = Num1 + Num2;
+            }
+            else if (Session["Operand"] != null && Session["Operand"].ToString() == "-")
+            {
+                res = Num1 - Num2;
+            }
+            else if (Session["Operand"] != null && Session["Operand"].ToString() == "x")
+            {
+                res = Num1 * Num2;
+            }
+            else if (Session["Operand"] != null && Session["Operand"].ToString() == "/")
+            {
+                res = Num1 / Num2;
+            }
+            else if (Session["Operand"] != null && Session["Operand"].ToString() == "!")
+            {
+
+                for (int i = 1; i <= Num1; i++)
+                {
+
+                    res = res * i;
+                }
+                
+            }
+            Label2.Text = res.ToString();
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (Session["Operand"] == null) Session["Num1"] += ".";
+            else Session["Num2"] += ".";
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+
+        protected void Button18_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+            if (Session["Num2"] != null && Session["Num2"].ToString() != "")
+            {
+                Session["Num2"] = Session["Num2"].ToString().Substring(0, Session["Num2"].ToString().Length - 1);
+            }
+            else if (Session["Operand"]!=null && Session["Operand"].ToString() != "") {
+                Session["Operand"] = Session["Operand"].ToString().Substring(0, Session["Operand"].ToString().Length - 1);
+            }
+            else if (Session["Num1"]!= null && Session["Num1"].ToString() != "")
+            {
+                Session["Num1"] = Session["Num1"].ToString().Substring(0, Session["Num1"].ToString().Length - 1);
+        }
+            if (Session["Num1"] != null) Label1.Text = Session["Num1"].ToString();
+            if (Session["Operand"] != null) Label1.Text += Session["Operand"].ToString();
+            if (Session["Num2"] != null) Label1.Text += Session["Num2"].ToString();
+        }
+    }
+}
